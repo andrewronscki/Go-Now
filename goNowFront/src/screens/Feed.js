daa6886495ec0316df82841a7ffc75dd03f564b7
andrewronscki <andrewronscki@gmail.com>
2019-01-30 21:58:41 -0200

    Commit 3

---
+++ goNowFront/src/screens/Feed.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from '../components/Header';
+
+class Feed extends Component {
+    render() {
+        return(
+            <View >
+                <Header />
+            </View>
+        );
+    }
+}
+export default Feed;
